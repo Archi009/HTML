9ed72381168785f78fdc5676b7f5a22bb820eb9d
Archi <dnrdl6817@gmail.com>
2025-01-20 00:04:55 +0900

    homework

--- javascript/function/event1.js
+++ javascript/function/event1.js
@@ -10,7 +10,7 @@ let clear = document.querySelector('#clear');
 plus.addEventListener('click', function () {
   // let num1 = document.getElementById('num1').value; value 값은 문자로 받는다. 
 
-  /* value를 쓰지 않고 아래 console.dif()을 사용하면 
+  /* value를 쓰지 않고 아래 console.dir()을 사용하면 
    input 태그의 모든 정보를 콘솔창에 가져온다 
    그곳에서 value의 여러 속성중 하나인 valuAsNumber을 찾아 숫자로 받아 사용할 수 있었다.
    만약 그냥 value로 사용하려 한다면 parseInt가 필요하다.*/
